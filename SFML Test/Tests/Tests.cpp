b9f06bb2950464bac1203cebe867234158c48078
Niklas Logren <niklas@niklaslogren.com>
2012-04-18 23:17:56 +0200

    Created new Tests project.

---
+++ SFML Test/Tests/Tests.cpp
@@ -0,0 +1,21 @@
+// Tests.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "gtest/gtest.h"
+
+TEST(sample_test_case, sample_test)
+{
+    EXPECT_EQ(1, 1);
+}
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	testing::InitGoogleTest(&argc, argv); 
+    RUN_ALL_TESTS(); 
+    std::getchar(); // keep console window open until Return keystroke
+
+	return 0;
+}
+
