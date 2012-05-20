0374f87b70977ef71e283ae5ac608f9653822870
Salking <Eaglus_@hotmail.com>
2012-05-20 18:23:29 +0200

    Started working on a host screen

--- SFML Test/SFML Test/view/HostGameScreen.h
+++ SFML Test/SFML Test/view/HostGameScreen.h
@@ -1,5 +1,5 @@
-#ifndef JOIN_GAME_SCREEN_H
-#define JOIN_GAME_SCREEN_H
+#ifndef HOST_GAME_SCREEN_H
+#define HOST_GAME_SCREEN_H
 
 #include "Screen.h"
 #include "GUIElements/Button.h"
@@ -8,30 +8,35 @@
 
 namespace mp
 {
-	class JoinGameScreen : public Screen
+	class HostGameScreen : public Screen
 	{
-		public:
-			JoinGameScreen(const sf::Vector2u &resolution);
-			virtual ~JoinGameScreen();
+		public: 
+			HostGameScreen(const sf::Vector2u &resolution);
+			virtual ~HostGameScreen();
 
 			virtual bool hover(const sf::Vector2i& mousePos);
 
 			virtual void update() {}
-		   // int click(const sf::Vector2i& mousePos) { return 1; };
+
 			virtual GUIElement* getElement(std::string element);
-		protected:
+
 		private:
 			sf::Font* font;
 			sf::Shape* background;
 
+			Button* hostButton;
+			Button* cancelButton;
+
 			std::map<std::string, GUIElement*> guiElements;
+
+			sf::Text* gravityTitleText;
+			sf::Text* portTitleText;
+			
 
 			sf::Text* screenTitleText;
 			sf::Text* ipTitleText;
-			sf::Text* portTitleText;
-
 
 			virtual void draw(sf::RenderTarget& window, sf::RenderStates states) const;
 	};
 }
-#endif // JOIN_GAME_SCREEN_H
+#endif
