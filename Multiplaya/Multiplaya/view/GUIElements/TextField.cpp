f497fda492bc49946ec5cf7cd797b20b51ea5f6a
Salking <Eaglus_@hotmail.com>
2012-05-23 01:14:40 +0200

    Added documentation for textfield and button

--- Multiplaya/Multiplaya/view/GUIElements/TextField.cpp
+++ Multiplaya/Multiplaya/view/GUIElements/TextField.cpp
@@ -15,6 +15,41 @@ namespace mp
 
 	}
 
+	/**
+	* Method that returns color of the border when it has been clicked
+	* @return color
+	*/
+	const sf::Color& TextField::getBorderColorClicked()
+	{
+		return borderColorClicked;
+	}
+
+	/**
+	* Set color of border when it's clicked
+	* @param color
+	*/
+	void TextField::setBorderColorClicked(const sf::Color& color)
+	{
+		borderColorClicked = color;
+	}
+
+	/**
+	* Method that returns thickness of the border when it has been clicked
+	* @return thickness
+	*/
+	float TextField::getBorderThicknessClicked()
+	{
+		return borderThicknessClicked;
+	}
+
+	/**
+	* Set thickness of border when it has been clicked
+	* @param thickness
+	*/
+	void TextField::setBorderThicknessClicked(float thickness) {
+		borderThicknessClicked = thickness;
+	}
+
 	bool TextField::isMouseOver(const sf::Vector2i& mousePos)
 	{
 		// if mouse is over
@@ -23,6 +58,10 @@ namespace mp
 		return false;
 	}
 
+	/**
+	* Method that checks if text field has been clicked
+	* @return true if has been clicked
+	*/
 	bool TextField::isClicked()
 	{
 		return clicked;
@@ -42,25 +81,5 @@ namespace mp
 			getBackground()->setOutlineColor(getBorderColor());
 			clicked = false;
 		}
-	}
-
-	void TextField::setBorderColorClicked(const sf::Color& color)
-	{
-		borderColorClicked = color;
-	}
-	const sf::Color& TextField::getBorderColorClicked()
-	{
-		return borderColorClicked;
-	}
-
-
-	float TextField::getBorderThicknessClicked()
-	{
-		return borderThicknessClicked;
-	}
-
-	
-	void TextField::setBorderThicknessClicked(float thickness) {
-		borderThicknessClicked = thickness;
 	}
 }
