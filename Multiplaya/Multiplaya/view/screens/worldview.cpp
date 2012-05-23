e824840aaaf5a6bfc6464e91ad4780df7aba364c
Jonathan Orro <jonathan.orro@gmail.com>
2012-05-23 21:58:42 +0200

    Added some mutex locks that were removed.

--- Multiplaya/Multiplaya/view/screens/worldview.cpp
+++ Multiplaya/Multiplaya/view/screens/worldview.cpp
@@ -52,7 +52,6 @@ namespace mp
 			worldViewMutex.lock();
 			Bullet* bullet = ( Bullet* )object;
 			addBullet(bullet);
-			std::cout<<bullet->getBody()->GetLinearVelocity().x<<std::endl;
 			worldViewMutex.unlock();
 		}
 		else if (e == BULLET_DELETED)
